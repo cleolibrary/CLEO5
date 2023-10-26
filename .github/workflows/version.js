c3091e37ed79a242fb2955f037fe439111478a6c
Seemann <mail@sannybuilder.com>
2023-10-25 21:54:26 -0400

    update

--- .github/workflows/version.js
+++ .github/workflows/version.js
@@ -5,10 +5,15 @@ const { GITHUB_OUTPUT, GITHUB_REF_NAME } = process.env;
 if (GITHUB_REF_NAME) {
   const version = GITHUB_REF_NAME.startsWith("v") ? GITHUB_REF_NAME.slice(1) : GITHUB_REF_NAME;
   addOutput("version", version);
+
+  // update cleo.h to replace version
+  const cleoH = readFileSync("cleo_sdk/cleo.h", { encoding: "utf-8" });
+  const newCleoH = cleoH.replace(/#define CLEO_VERSION_STR .*/, `#define CLEO_VERSION_STR "${version}"`);
+  writeFileSync("cleo_sdk/cleo.h", newCleoH, { encoding: "utf-8" });
 }
 
 const changelog = readFileSync("CHANGELOG.md", { encoding: "utf-8" });
-writeFileSync('changes.txt',  getChanges().join(EOL), { encoding: 'utf-8' });
+writeFileSync("changes.txt", getChanges().join(EOL), { encoding: "utf-8" });
 
 function addOutput(key, value) {
   appendFileSync(GITHUB_OUTPUT, `${key}=${value}${EOL}`, { encoding: "utf-8" });
