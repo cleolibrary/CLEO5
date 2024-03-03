a82fabb298f3155a5a5e1d77f0fce59715dcb30f
Seemann <mail@sannybuilder.com>
2024-03-03 01:16:34 -0500

    update

--- tests/cleo_tests/compile.bat
+++ tests/cleo_tests/compile.bat
@@ -1,4 +1,4 @@
-@REM Compile all .txt files in the current directory to .cs files using Sanny Builder 4
+@REM Compile all .txt files in the current directory to .s files using Sanny Builder 4
 @REM Usage: set SANNY="path\to\sanny.exe" && compile.bat
 
-for /f "delims=" %%i in ('dir /b /s *.txt') do %SANNY% --compile "%%i" "%%~pi%%~ni.cs" --no-splash --mode sa_sbl
+for /f "delims=" %%i in ('dir /b /s *.txt') do %SANNY% --compile "%%i" "%%~pi%%~ni.s" --no-splash --mode sa_sbl
