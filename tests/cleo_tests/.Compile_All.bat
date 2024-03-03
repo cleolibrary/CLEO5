0a3f774b0f464fd1a4de548306394cc73504dc51
Seemann <mail@sannybuilder.com>
2024-03-03 12:14:27 -0500

    review

---
+++ tests/cleo_tests/.Compile_All.bat
@@ -0,0 +1,19 @@
+@REM Compile all .txt files in the current directory to .s files using Sanny Builder 4
+@REM Usage: set SANNY="path\to\sanny.exe" && .Compile_All.bat
+
+@echo off
+
+@REM Delete all .s files in the current directory and subdirectories
+for /f "delims=" %%i in ('dir /b /s *.s') do (
+    echo Deleting "%%i"...
+    del "%%i"
+)
+
+@REM Compile all .txt files in the current directory and subdirectories
+for /f "delims=" %%i in ('dir /b /s *.txt') do (
+    echo Compiling "%%i"...
+    %SANNY% --compile "%%i" "%%~pi%%~ni.s" --no-splash --mode sa_sbl
+)
+
+echo Done.
+pause
