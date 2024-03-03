4ed922d5990c4ef803e48e30ad0528bfe363bbdd
Seemann <mail@sannybuilder.com>
2024-03-03 14:04:07 -0500

    update

--- tests/cleo_tests/.Compile_All.bat
+++ tests/cleo_tests/.Compile_All.bat
@@ -2,17 +2,20 @@
 @REM Usage: set SANNY="path\to\sanny.exe" && .Compile_All.bat
 
 @echo off
+SETLOCAL EnableDelayedExpansion
 
 @REM Delete all .s files in the current directory and subdirectories
 for /f "delims=" %%i in ('dir /b /s *.s') do (
-    echo Deleting "%%~ni%%~xi"...
+    set p=%%i 
+    echo Deleting !p:%__CD__%=!...
     del "%%i"
 )
 
 @REM Compile all .txt files in the current directory and subdirectories
 for /f "delims=" %%i in ('dir /b /s *.txt') do (
-    echo Compiling "%%~ni%%~xi"...
-    %SANNY% --compile "%%i" "%%~dpi%%~ni.s" --no-splash --mode sa_sbl
+    set p=%%i 
+    echo Compiling !p:%__CD__%=!...
+    %SANNY% --compile "%%i" "%%~dpni.s" --no-splash --mode sa_sbl
 )
 
 echo Done.
