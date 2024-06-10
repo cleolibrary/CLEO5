8729074c605f12e69de06fe67aeee8817d93f51d
Miran <Miran4@op.pl>
2024-06-10 09:20:39 +0200

    fixup! Removed VirusTotal step

--- source/CCustomOpcodeSystem.cpp
+++ source/CCustomOpcodeSystem.cpp
@@ -963,7 +963,7 @@ namespace CLEO
 		DWORD key;
 		*thread >> key;
 
-		SHORT(__stdcall * GTA_GetKeyState)(int nVirtKey) = memory_pointer(0x0081E64C); // use ingame function as GetKeyState might look like keyloger to some AV software
+		SHORT(__stdcall * GTA_GetKeyState)(int nVirtKey) = memory_pointer(0x0081E64C); // use ingame function as GetKeyState might look like keylogger to some AV software
 		bool isDown = (GTA_GetKeyState(key) & 0x8000) != 0;
 
 		SetScriptCondResult(thread, isDown);
