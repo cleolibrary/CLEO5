6c8866919275ec2fbf14b147e3b47327d5121874
Miran <Miran4@op.pl>
2024-09-20 00:47:56 +0200

    fixup! Switched hooked function to work with SkyGFX

--- source/CGameVersionManager.h
+++ source/CGameVersionManager.h
@@ -86,7 +86,7 @@ namespace CLEO
         MA_CALL_GAME_RESTART_1,
         MA_CALL_GAME_RESTART_2,
         MA_CALL_GAME_RESTART_3,
-        MA_FLUSH_OBRS_PRINTFS, // empty function called after everything else is drawn
+        MA_DEBUG_DISPLAY_TEXT_BUFFER, // empty function called after everything else is drawn
 
         // CustomOpcodeSystem
         MA_OPCODE_HANDLER,
