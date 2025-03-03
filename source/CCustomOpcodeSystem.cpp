44f42b2174a42ad490737c5e7e5ba5b4f9cf6bf9
Miran <Miran4@op.pl>
2025-03-03 19:16:17 +0100

    Added COMPAT_MODE_TIP macro.

--- source/CCustomOpcodeSystem.cpp
+++ source/CCustomOpcodeSystem.cpp
@@ -112,7 +112,7 @@ namespace CLEO
 			auto endPos = cs->GetBasePointer() + cs->GetCodeSize();
 			if ((BYTE*)lastOpcodePtr == endPos || (BYTE*)lastOpcodePtr == (endPos - 1)) // consider script can end with incomplete opcode
 			{
-				SHOW_ERROR("Code execution past script end in script %s\nThis usually happens when [004E] command is missing.\nScript suspended.\n\nTo ignore this error, change the file extension from .cs to .cs4 and restart the game.", ((CCustomScript*)thread)->GetInfoStr().c_str());
+				SHOW_ERROR(COMPAT_MODE_TIP("Code execution past script end in script %s\nThis usually happens when [004E] command is missing.\nScript suspended."), ((CCustomScript*)thread)->GetInfoStr().c_str());
 				return thread->Suspend();
 			}
 		}
