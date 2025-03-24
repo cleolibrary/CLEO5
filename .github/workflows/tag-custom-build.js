f01f871cb8421373c79dad8b4e3b88ebd4c11d04
Seemann <x87@users.noreply.github.com>
2025-03-24 14:00:31 -0400

    fix

--- .github/workflows/tag-custom-build.js
+++ .github/workflows/tag-custom-build.js
@@ -1,3 +1,4 @@
+const { readFileSync, writeFileSync } = require("fs");
 const { GITHUB_SHA } = process.env;
 
 if (GITHUB_SHA) {
@@ -7,5 +8,6 @@ if (GITHUB_SHA) {
   const cleoH = readFileSync("cleo_sdk/cleo.h", { encoding: "utf-8" });
   const newCleoH = cleoH
     .replace('(CLEO_VERSION_MAIN.CLEO_VERSION_MAJOR.CLEO_VERSION_MINOR)', `(CLEO_VERSION_MAIN.CLEO_VERSION_MAJOR.CLEO_VERSION_MINOR)"-${sha}"`);
+  console.log(`Tagging current build with sha ${sha}`);
   writeFileSync("cleo_sdk/cleo.h", newCleoH, { encoding: "utf-8" });
 }
