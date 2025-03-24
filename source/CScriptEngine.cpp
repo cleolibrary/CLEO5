849a252d86df41ffec54f1aa4529b91d71f25be7
Miran <Miran4@op.pl>
2025-03-24 01:41:18 +0100

    Less casting, mirrored code in other branches.

--- source/CScriptEngine.cpp
+++ source/CScriptEngine.cpp
@@ -146,18 +146,19 @@ namespace CLEO
         // integer address to text buffer
         if (IsImmInteger(paramType) || isVariableInt)
         {
-            auto addr = CLEO_PeekIntOpcodeParam(thread);
+            auto str = (char*)CLEO_PeekIntOpcodeParam(thread);
             CLEO_SkipOpcodeParams(thread, 1);
-            if (addr <= CCustomOpcodeSystem::MinValidAddress)
+
+            if ((size_t)str <= CCustomOpcodeSystem::MinValidAddress)
             {
-                LOG_WARNING(thread, "Invalid '0x%X' pointer of input string argument %s in script %s", addr, GetParamInfo().c_str(), ScriptInfoStr(thread).c_str());
+                LOG_WARNING(thread, "Invalid '0x%X' pointer of input string argument %s in script %s", str, GetParamInfo().c_str(), ScriptInfoStr(thread).c_str());
                 return nullptr; // error
             }
 
-            auto len = min((int)strlen((char*)addr), buffLen);
-            memcpy(buff, (char*)addr, len);
+            auto len = min((int)strlen(str), buffLen);
+            memcpy(buff, str, len);
             if (len < buffLen) buff[len] = '\0'; // add terminator if possible
-            return (char*)addr; // pointer to original data
+            return str; // pointer to original data
         }
         else if (paramType == DT_VARLEN_STRING)
         {
