f33ea19b691a6ce7d9fc7e4b6fe2699859be161b
Miran <Miran4@op.pl>
2025-06-29 17:44:50 +0200

    fixup! Fix condition result of cleo_call used in if statements as 7th or later expression

--- cleo_sdk/CLEO.h
+++ cleo_sdk/CLEO.h
@@ -258,7 +258,6 @@ enum eLogicalOperation : WORD
 	AND_6,
 	AND_7,
 	AND_8,
-	AND_9,
 	AND_END,
 	
 	OR_2 = 21, // OR operation on results of next two conditional opcodes
@@ -268,7 +267,6 @@ enum eLogicalOperation : WORD
 	OR_6,
 	OR_7,
 	OR_8,
-	OR_9,
 	OR_END,
 };
 static eLogicalOperation& operator--(eLogicalOperation& o)
