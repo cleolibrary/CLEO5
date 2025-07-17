5a03a89c8dbb2ff1a099c1377119bd30f093c733
Miran <Miran4@op.pl>
2025-07-17 05:04:20 +0200

    Review fixes

--- cleo_plugins/MemoryOperations/MemoryOperations.cpp
+++ cleo_plugins/MemoryOperations/MemoryOperations.cpp
@@ -222,8 +222,11 @@ public:
         CLEO_SkipUnusedVarArgs(thread);
 
         // set condition result
-        if (!returnArg) result &= 0xFF; // clip to AL
-        OPCODE_CONDITION_RESULT(result != 0);
+        if (!IsLegacyScript(thread))
+        {
+            if (!returnArg) result &= 0xFF; // clip to AL
+            OPCODE_CONDITION_RESULT(result != 0);
+        }
 
         return OR_CONTINUE;
     }
