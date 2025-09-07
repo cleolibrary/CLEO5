f71980b6d7b5a4cf30736ae5b71767f7439552f9
Seemann <mail@sannybuilder.com>
2025-09-07 09:37:04 -0400

    update script

--- clang-format.bat
+++ clang-format.bat
@@ -3,10 +3,14 @@ echo Formatting all C/C++ files using clang-format...
 
 setlocal enabledelayedexpansion
 
+set "ignore_files="
+for /f "delims=" %%i in (./.clang-format-ignore) do (
+    set "ignore_files=!ignore_files! %%i"
+)
 
 for %%e in (cpp h c hpp) do (
     for /r %%f in (*.%%e) do (
-        echo %%f | findstr /i "third-party" >nul
+        echo %%f | findstr /i "!ignore_files!" >nul
         if !errorlevel! neq 0 (
             clang-format.exe -i --verbose "%%f"
         )
