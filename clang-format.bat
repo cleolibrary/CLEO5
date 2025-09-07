9d99b33c3ed0cdd25da0c5d3105a63f8a03067d3
Seemann <mail@sannybuilder.com>
2025-09-07 10:03:48 -0400

    update script

--- clang-format.bat
+++ clang-format.bat
@@ -7,10 +7,12 @@ set "ignore_files="
 for /f "delims=" %%i in (./.clang-format-ignore) do (
     set "ignore_file=%%i"
 
-    @rem Convert Unix path to Windows
-    set "ignore_file=!ignore_file:/=\!"
-    set "ignore_file=!ignore_file:~1!"
-    set "ignore_files=!ignore_files! !ignore_file!"
+    if not "!ignore_file!"=="" if not "!ignore_file:~0,1!"=="#" (
+        @rem Convert Unix path to Windows
+        set "ignore_file=!ignore_file:/=\!"
+        set "ignore_file=!ignore_file:~1!"
+        set "ignore_files=!ignore_files! !ignore_file!"
+    )
 )
 
 echo Ignored files:
