2f592abf64856c9d65fa6969f5f46b403b54dcdf
Miran <Miran4@op.pl>
2025-10-11 14:18:20 +0200

    Added stack overflow error prevention for GOSUB commands

--- source/CCustomOpcodeSystem.cpp
+++ source/CCustomOpcodeSystem.cpp
@@ -133,6 +133,7 @@ namespace CLEO
 		TRACE("Initializing CLEO core opcodes...");
 
 		CLEO_RegisterOpcode(0x004E, opcode_004E);
+		CLEO_RegisterOpcode(0x0050, opcode_0050);
 		CLEO_RegisterOpcode(0x0051, opcode_0051);
 		CLEO_RegisterOpcode(0x0417, opcode_0417);
 		CLEO_RegisterOpcode(0x0A92, opcode_0A92);
@@ -674,6 +675,20 @@ namespace CLEO
 		return OR_INTERRUPT;
 	}
 
+	// gosub
+	// gosub [label]
+	OpcodeResult __stdcall CCustomOpcodeSystem::opcode_0050(CRunningScript* thread)
+	{
+		constexpr auto Stack_Size = _countof(CRunningScript::Stack);
+		if (thread->SP >= Stack_Size)
+		{
+			SHOW_ERROR("Call stack overflow in script %s\nMax up to %d nested gosub calls is supported.\nScript suspended.", ScriptInfoStr(thread).c_str(), Stack_Size);
+			return thread->Suspend();
+		}
+
+		return CallNativeOpcode(thread, 0x0050); // call game's original
+	}
+
 	// GOSUB return
 	OpcodeResult __stdcall CCustomOpcodeSystem::opcode_0051(CRunningScript* thread)
 	{
@@ -787,6 +802,13 @@ namespace CLEO
 		auto offset = OPCODE_READ_PARAM_INT();
 
 		if (thread->GetConditionResult()) return OR_CONTINUE;
+
+		constexpr auto Stack_Size = _countof(CRunningScript::Stack);
+		if (thread->SP >= Stack_Size)
+		{
+			SHOW_ERROR("Call stack overflow in script %s\nMax up to %d nested gosub calls is supported.\nScript suspended.", ScriptInfoStr(thread).c_str(), Stack_Size);
+			return thread->Suspend();
+		}
 
 		thread->PushStack(thread->GetBytePointer());
 		thread->Jump(offset);
