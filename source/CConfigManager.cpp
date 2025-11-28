d874af4c94a36506d5fd8421210e9fd3686d7272
Seemann <mail@sannybuilder.com>
2025-11-28 14:32:45 -0500

    delete inline comments

--- source/CConfigManager.cpp
+++ source/CConfigManager.cpp
@@ -19,6 +19,11 @@ namespace CLEO
         // Read from file and then cache
         char buffer[512];
         GetPrivateProfileString(section, key, "", buffer, sizeof(buffer), GetConfigPath());
+        
+        // remove comments starting with ;
+        char* commentPos = strchr(buffer, ';');
+        if (commentPos) *commentPos = '\0';
+
         return cache[cacheKey] = buffer;
     }
 
